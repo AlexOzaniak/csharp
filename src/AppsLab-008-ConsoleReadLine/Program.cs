a9e688547815f0e9110f7fa4832fb960da09154d
Alex Ozaniak <alexozaniakk@gmail.com>
2025-09-24 12:50:11 +0200

    opravil som chyby

--- src/AppsLab-008-ConsoleReadLine/Program.cs
+++ src/AppsLab-008-ConsoleReadLine/Program.cs
@@ -1,8 +1,6 @@
-﻿Console.WriteLine("Ako sa voláš?");
-string? meno = Console.ReadLine();
-Console.WriteLine("Ahoj, " + meno);
-
-Console.WriteLine("Koľko máš rokov?");
-string vstup = Console.ReadLine() ?? "0";
-int vek = int.Parse(vstup);
-Console.WriteLine("Máš " + vek + " rokov.");
+﻿
+Console.WriteLine("ako sa volas?");
+string ? meno = Console.ReadLine();
+Console.WriteLine($"Ahoj {meno} Kolko mas rokov?");
+int vek = int.Parse(Console.ReadLine());
+Console.WriteLine($"AHA mas teda {vek} rokov");
