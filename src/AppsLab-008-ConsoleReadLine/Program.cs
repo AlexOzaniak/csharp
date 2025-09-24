8ad432352a0cf8144ee7c5031238e66cfcbd08ce
Alex Ozaniak <alexozaniakk@gmail.com>
2025-09-24 13:36:57 +0200

    bonsova uloha

--- src/AppsLab-008-ConsoleReadLine/Program.cs
+++ src/AppsLab-008-ConsoleReadLine/Program.cs
@@ -7,6 +7,15 @@ Console.WriteLine($"AHA mas teda {vek} rokov");
 Console.WriteLine("ake je tvoje oblubene jedlo ?");
 string jedlo = Console.ReadLine();
 Console.WriteLine($" hmm {jedlo} znie skvele");
+Console.WriteLine("stlac klavesu :");
+var key = Console.ReadKey();
+Console.WriteLine();
+Console.WriteLine($"stlacili ste klavesu: {key.KeyChar}");
+
+
+
+
+
 
 
 
