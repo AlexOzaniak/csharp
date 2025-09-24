8d12c45f8728b19f34654a2a6555026839392732
Alex Ozaniak <alexozaniakk@gmail.com>
2025-09-24 12:59:20 +0200

    pridal som otazku

--- src/AppsLab-008-ConsoleReadLine/Program.cs
+++ src/AppsLab-008-ConsoleReadLine/Program.cs
@@ -4,3 +4,6 @@ string ? meno = Console.ReadLine();
 Console.WriteLine($"Ahoj {meno} Kolko mas rokov?");
 int vek = int.Parse(Console.ReadLine());
 Console.WriteLine($"AHA mas teda {vek} rokov");
+Console.WriteLine("ake je tvoje oblubene jedlo ?");
+string jedlo = Console.ReadLine();
+Console.WriteLine($" hmm {jedlo} znie skvele");
